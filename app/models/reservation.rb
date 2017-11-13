64b661808af4ad01056fa42e4c44bfaadb9c897d
RochelLevi <rochlevi@gmail.com>
2017-11-13 17:37:48 -0500

    fixed has_one relationship

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,7 +2,8 @@ class Reservation < ActiveRecord::Base
   belongs_to :listing
   belongs_to :guest, :class_name => "User"
   #why has many?
-  has_many :reviews
+  #has_many :reviews
+  has_one :review
 
 
 end
